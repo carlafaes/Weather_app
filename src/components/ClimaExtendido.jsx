996469894b53cf8f78b6e995569caaf8e0b49533
carlafaes <carlafaes18@hotmail.com>
2022-07-06 00:18:31 -0300

    infromacion astronomica

--- src/components/ClimaExtendido.jsx
+++ src/components/ClimaExtendido.jsx
@@ -15,8 +15,24 @@ export default function ClimaExtendido({weather}){
                         <p><BsArrowDownSquareFill/>{day.day.mintemp_c}º</p>
                         <p><BsArrowUpSquareFill/>{day.day.maxtemp_c}º</p>
                     </div>
-                    <img src={`http:${day.day.condition.icon}`}/>
+                    <div>
+                        <p>Viento {day.day.maxwind_kph}km/h</p>
+                        <p>Humedad {day.day.avghumidity}%</p>
+                        <p>Precipitaciones {day.day.totalprecip_mm}mm</p>
+                    </div>
+                    <img src={`http:${day.day.condition.icon}`} alt={day.day.condition.text}/>
                     <p>{day.day.condition.text}</p>
+                    <div>
+                        <h3>Informacion astronómica</h3>
+                        <h4>Luna y fases de la luna</h4>
+                        <p>Fase de la luna:{day.astro.moon_phase}</p>
+                        <p>Puesta de luna:{day.astro.moonset}</p>
+                        <p>Salida de la luna: {day.astro.moonrise}</p>
+                        <br/>
+                        <h4>Sol y luz</h4>
+                        <p>Amanecer:{day.astro.sunrise}</p>
+                        <p>Atardecer:{day.astro.sunset}</p>
+                    </div>
                 </div>
             ))
         : 
