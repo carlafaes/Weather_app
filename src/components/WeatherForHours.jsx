a75c23f70f09ac4455c338a054fd260239a0cc09
carlafaes <carlafaes18@hotmail.com>
2022-07-06 18:50:41 -0300

    nnn

--- src/components/WeatherForHours.jsx
+++ src/components/WeatherForHours.jsx
@@ -1,41 +1,44 @@
-
+import { NavItem } from 'react-bootstrap'
+import Table from 'react-bootstrap/Table'
 
 export default function WeatherForHours({weather}) {
-let horas=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-
 
 return(
         <div>
             <h2>Pronostico por horas</h2>
+            <Table responsive="sm" striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>Horas</th>
+                        <th>Temperatura</th>
+                    </tr>
+                </thead>
             {weather ? weather.forecast.forecastday.map(function(hours,index){
                 return(
-                    <div key={index}>
-                        <div>{hours.hour.map(function(item,index){
+                    <>
+                        {hours.hour.map(function(hour,index){
                             return(
-                                <div key={index}>
-                                    <table>
-                                        <thead>
-                                            {index +1 }
-                                        </thead>
-                                        <tr>
-                                            <td>{item.feelslike_c}</td>
-                                        </tr>  
-                                        <tr>
-                                            <td>{item.condition.text}</td>
-                                            <td>
-                                            <img src={`http:${item.condition.icon}`} alt={item.condition.text}/>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
+                                <tbody>
+                                <tr>
+                                    <td>{index +1}</td>
+                                    <td>{hour.feelslike_c}</td>
+                            
+                                    <td>
+                                        {hour.condition.text}
+                                        <img src={`http:${hour.condition.icon}`} alt={hour.condition.text}/>
+                                    </td>
+                                    
+
+                                    
+                                </tr>
+                                </tbody>
                             )
-                        }
-                        )}</div>
-                    </div>
-                )
-            })
+                        })}
+                    </>
+                )})
             :
-            <div>Buscando clima</div>}
+            <div>Buscando pronostico</div>}
+            </Table>
         </div>
     )
 }
