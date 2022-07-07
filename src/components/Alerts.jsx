bdefa37da9ac89651e3e1688dd99411a061ccb28
carlafaes <carlafaes18@hotmail.com>
2022-07-07 02:04:35 -0300

    alertas met

--- src/components/Alerts.jsx
+++ src/components/Alerts.jsx
@@ -1,7 +1,38 @@
+import Table from 'react-bootstrap/Table'
 
 export default function Alerts({weather}){
 
     return(
-        <div>alertas</div>
+        <div>
+            {weather? weather.alerts.alert.map((alert,index)=>(
+                <div key={index}>
+                   <Table responsive striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>Titular</th>
+                            <th>Categoria</th>
+                            <th>Evento</th>
+                            <th>Descripcion</th>
+                            <th>Gravedad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{alert.headline ? alert.headline : "No se registra"}</td>
+                            <td>{alert.category ? alert.category : "No se registra"}</td>
+                            <td>{alert.event ? alert.event : "No se registra"}</td>
+                            <td>{alert.desc ? alert.desc : "No se registra"}</td>
+                            <td>{alert.severity ? alert.severity : "No se registra"}</td>
+                        </tr>
+                    </tbody>
+                   </Table>
+                </div>
+                
+            ))
+            :
+            <div>
+                <p>No se registran Alertas Metereologicas</p>
+            </div>}
+        </div>
     )
 }
